81b9df6e139f8e7cb4ff21660d8b9ef165e34aa5
刘涛 <2394630102@qq.com>
2024-08-16 10:45:50 +0800

    commit

--- note/index.js
+++ note/index.js
@@ -1,50 +1,6 @@
-const fn = () => {
-  return "llllll";
-};
-const target = {
-  h: { name: "wad" },
-  j: [1, 2, 3],
-  fn,
-};
-target.j.push(target.j);
-target.i = target;
-const myCopy = (target) => {};
-const myDeepCopy = (target, map = new WeakMap()) => {
-  const type = Object.prototype.toString.call(target).slice(8, -1);
-  console.log({ type, target });
-  if (type === "Object") {
-    let ret = {};
-    const has = map.has(target);
-    if (has) {
-      ret = map.get(target);
-    } else {
-      map.set(target, target);
-      for (const key in target) {
-        if (target.hasOwnProperty(prop)) {
-          ret[key] = myDeepCopy(target[key], map);
-        }
-      }
-    }
-    return ret;
-  } else if (type === "array") {
-    let ret = [];
-    const has = map.has(target);
-    if (has) {
-      ret = map.get(target);
-    } else {
-      map.set(target, target);
-      for (const item of target) {
-        ret.push(myDeepCopy(item, map));
-      }
-    }
-    return ret;
-  } else {
-    return target;
-  }
-};
-const normal = myCopy(target);
-const deep = myDeepCopy(target);
-target.fn = () => "222222222222";
-console.log(target.fn());
-console.log(deep.fn());
-console.log({ target, normal, deep });
+function myNew(func, ...args) {
+  const obj = {};
+  obj.__proto__ = func.prototype;
+  const result = func.apply(obj, args);
+  return result instanceof Object ? result : obj;
+}
